0adbda46a3cb1047b39ff3f86e22b280a3c68396
Renan Correia <renancorreia730@gmail.com>
2025-08-08 11:29:23 -0300

    Add files via upload

---
+++ ATIVIDADE 1 POO/aula1.java
@@ -0,0 +1,8 @@
+public class aula1 {
+    public static void main(String[] args) {
+        String nome = "Renan";
+        int idade = 20;
+        float salario  = 200;
+        System.out.println(nome+" tem "+idade+" anos e ganha "+salario+" reais.");
+    }
+}
